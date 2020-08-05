6ea6a7c491f6b93c13d9b8b1a08843c4bb104031
zfnmxt <zfnmxt@zfnmxt.com>
2020-08-05 14:33:25 +0200

    Oops

--- shell.nix
+++ shell.nix
@@ -3,3 +3,4 @@ with nixpkgs;
 stdenv.mkDerivation {
   name = "futhark-ad";
   buildInputs = [ zlib zlib.out pkgconfig haskell.compiler.ghc883 cabal-install hlint (python38.withPackages(ps: with ps; [ numpy toolz ]))];
+}
